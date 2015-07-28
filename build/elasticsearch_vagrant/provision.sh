9365e3672694445c7afd32fb4f52338dbf2c4177
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-07-28 16:44:34 +0300

    fix(elasticsearch_vagrant): fix script to load elasticsearch

--- build/elasticsearch_vagrant/provision.sh
+++ build/elasticsearch_vagrant/provision.sh
@@ -42,7 +42,8 @@ if [ ! -f /etc/init.d/elasticsearch ]; then
     echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
     sudo apt-get update
     sudo apt-get -y install elasticsearch=1.4.4
-    sudo mv elasticsearch.yml /etc/elasticsearch
+
+    sudo mv /vagrant/elasticsearch.yml /etc/elasticsearch
     sudo service elasticsearch restart
     sudo update-rc.d elasticsearch defaults 95 10
 
